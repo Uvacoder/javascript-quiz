250fb88c68d8e090eed2029acd36ae9fddf3a5a6
Maeling <maelingmurphy@users.noreply.github.com>
2021-03-03 20:00:01 -0500

    Add local directory files to repo

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+const quizQuestions = [
+    {
+        id: 1,
+        question: "Which method removes the last element from an array and returns that element?",
+        choices: [
+            "pop()",
+            "shift()",
+            "unshift()",
+            "slice()"
+        ],
+        answer: "pop()"
+    },
+    {
+        id: 2,
+        question: "Which method adds a new item to the end of an array?",
+        choices: [
+            "unshift()",
+            "slice()",
+            "push()",
+            "pop()"
+        ],
+        answer: "push()"
+    },
+    {
+        id: 3,
+        question: "Which method creates a new array based on a selection?",
+        choices: [
+            "shift()",
+            "appendChild()",
+            "slice()",
+            "push()"
+        ],
+        answer: "slice()"
+    },
+    {
+        id: 4,
+        question: "Which method adds a new item to the beginning of an array?",
+        choices: [
+            "unshift()",
+            "pop()",
+            "shift()",
+            "slice()"
+        ],
+        answer: "unshift()"
+    },
+    {
+        id: 5,
+        question: "Which method removes the first element from an array and returns that element?",
+        choices: [
+            "push()",
+            "slice()",
+            "unshift()",
+            "shift()"
+        ],
+        answer: "shift()"
+    }
+];
